81499cafb7459cfe517cf744693e919c30830204
tin.keita <ipb23.t.paegle@vtdt.edu.lv>
2025-09-03 23:21:25 +0300

    salikti grid elementi pa vietām

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php $data = file_get_contents("https://emo.lv/weather-api/forecast/?city=cesis,latvia");?>
+<?php $weatherData = json_decode($data, true);?>
+<?php echo "Pilsēta: " . $weatherData['city']['name']?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,12 +11,26 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php $data = file_get_contents("https://emo.lv/weather-api/forecast/?city=cesis,latvia");?>
-    <?php $weatherData = json_decode($data, true);?>
-    <?php echo "Pilsēta: " . $weatherData['city']['name']?>
-
     <div class="container">
-        
+        <div class="header">
+            <div class="name">VTDT sky</div>
+            <div class="search">
+                 <input type="text" placeholder="Search...">
+            </div>
+            <div class="toogle"></div>
+        </div>
+        <div class="weather">Current weather</div>
+        <div class="forecast">Forecast</div>
+        <div class="small">
+            <div class="air">Air</div>
+            <div class="wind">Wind</div>
+            <div class="humidity">Humitidy</div>
+            <div class="visibility">Visibility</div>
+            <div class="pressure_in">Pressure in</div>
+            <div class="pressure">Pressure</div>
+        </div>
+        <div class="sunmoon">Sun & Moon</div>
+
     </div>
 
 </body>
